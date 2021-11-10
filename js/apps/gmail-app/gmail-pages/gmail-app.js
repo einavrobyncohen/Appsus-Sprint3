5b094dd8e96b424044177687534d7a4ccd51ec58
Einav Cohen <einavsprojects@gmail.com>
2021-11-10 20:11:57 +0200

    forth commit

--- js/apps/gmail-app/gmail-pages/gmail-app.js
+++ js/apps/gmail-app/gmail-pages/gmail-app.js
@@ -1,6 +1,44 @@
+import { gmailService } from "../services/gmail-service.js"
+import emailList from "../gmail-cmps/email-list.cmp.js"
+import emailFilter from "../gmail-cmps/email-filter.cmp.js"
+
 
 export default {
+    components: {
+        emailList,
+        emailFilter
+    },
+
     template:`<section class="gmail-app app-main">
-        <h1>Hi Gmail App</h1>
+        <email-filter :unread="amountUnread"/>
+        <email-list :emails="emailsToShow"/>
     </section>`
+    ,
+
+    data() {
+        return {
+            emails: null,
+            filterBy: null,
+            amountUnread: 4
+
+        }
+    },
+    created() {
+        this.loadEmails()
+        
+    },
+    methods: {
+        loadEmails() {
+            gmailService.query()
+                .then(emails => {
+                    this.emails = emails
+                })
+        }
+    },
+    computed: {
+        emailsToShow() {
+            if (!this.filterBy) return this.emails
+
+        }
+    }
 }
--- js/apps/gmail-app/services/gmail-service.js
+++ js/apps/gmail-app/services/gmail-service.js
@@ -1,15 +1,92 @@
+import {utilService} from '/js/services/util-service.js'
+import {storageService} from '/js/services/async-storage-service.js'
+
+
+const EMAILS_KEY ='emails'
+_createEmails();
+
 
 export const gmailService = {
-
+    query,
+    getEmailById,
+    changeEmailToRead,
 }
 
-const email = {
-    id: 'e101',
-    subject: 'Miss you!',
-    body: 'Would love to catch up sometimes',
-    isRead: false,
-    sentAt : 1551133930594,
-    to: 'momo@momo.com'
+
+function query() {
+    return storageService.query(EMAILS_KEY)
+}
+
+function getEmailById(emailId) {
+    return storageService.get(EMAILS_KEY,emailId)
+}
+
+function changeEmailToRead(emailId) {
+    return getEmailById(emailId)
+        .then(email => {
+            email.isRead = true;
+            return storageService.put(EMAILS_KEY,emailId)
+        })
+}
+
+
+function _createEmails() {
+    var emails = [
+        {
+            id: 'e101',
+            sender: 'Muki',
+            subject: 'This is just a random subject',
+            body: 'Would love to catch up sometimes, its been a while!',
+            isRead: false,
+            sentAt : 1551133930594,
+            to: 'user@appsus.com',
+            from: 'sheker@mimi.com'
+        },
+        {
+            id: 'e102',
+            sender: 'Mahatma Appsus',
+            subject: 'Miss you!',
+            body: 'How are you? im so hungry what can i do im always eating',
+            isRead: false,
+            sentAt : 1551133930594,
+            to: 'momo@momo.com',
+            from: 'user@appsus.com'
+        },
+        {
+            id: 'e103',
+            sender: 'Shuki',
+            subject: 'Ani Aohevet Et Shlomo Artzi',
+            body: 'yesh li isha zot haima shlchaaaaaaaaaaaaa lalalalaa',
+            isRead: false,
+            sentAt : 1551133930594,
+            to: 'user@appsus.com',
+            from: 'mize@ani.com'
+        },
+        {
+            id: 'e104',
+            sender: 'Luki',
+            subject: 'BA LI PIZZA ZEITIM',
+            body: 'IM LECHEM SHUM, HARBEEEE GVINA IGLIDA',
+            isRead: false,
+            sentAt : 1551133930594,
+            to: 'user@appsus.com',
+            from: 'mize@ani.com'
+        },
+        {
+            id: 'e105',
+            sender: 'Luki',
+            subject: 'BA LI PIZZA ZEITIM',
+            body: 'IM LECHEM SHUM, HARBEEEE GVINA IGLIDA',
+            isRead: false,
+            sentAt : 1551133930594,
+            to: 'user@appsus.com',
+            from: 'mize@ani.com'
+        }
+
+    ]
+
+    utilService.saveToStorage(EMAILS_KEY, emails)
+    return emails
 }
 
 const loggedinUser = {
