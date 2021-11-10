795d53512883c8d108f236897e3f8b1fec8c9bef
Michal <michalozeri88@gmail.com>
2021-11-10 20:08:56 +0200

    third commit

--- js/apps/keep-app/keep-pages/keep-app.js
+++ js/apps/keep-app/keep-pages/keep-app.js
@@ -1,6 +1,53 @@
+import { keepService } from '../services/keep-service.js';
+import keepList from '../keep-cmps/keep-list.js';
 
 export default {
-    template:`<section class="app-main">
+    template: `
+    <section class="app-main">
     <h1>Keep App</h1>
-    </section>`
+    <keep-list :notes="notes" @remove="removeNote"></keep-list>
+    </section>
+    `,
+
+
+    data() {
+        return {
+            notes: null
+        };
+    },
+    created() {
+        this.loadNotes();
+    },
+    methods: {
+        loadNotes() {
+            keepService.query()
+                .then(notes => {
+                    this.notes = notes;
+                    console.log(this.notes);
+                });
+        },
+        removeNote(id) {
+            keepService.remove(id)
+                .then(() => {
+                    // const msg = {
+                    //     txt: 'Deleted succesfully',
+                    //     type: 'success'
+                    // };
+                    // eventBus.$emit('showMsg', msg);
+                    this.notes = this.notes.filter(note => note.id !== id)
+                })
+                // .catch(err => {
+                //     console.log('err', err);
+                //     const msg = {
+                //         txt: 'Error. Please try later',
+                //         type: 'error'
+                //     };
+                //     eventBus.$emit('showMsg', msg);
+                // });
+        },
+    },
+    computed: {},
+    components: {
+        keepList,
+    }
 }
---
+++ js/apps/keep-app/services/keep-service.js
@@ -0,0 +1,78 @@
+import { utilService } from '/js/services/util-service.js';
+import { storageService } from '/js/services/async-storage-service.js';
+
+const NOTES_KEY = 'notes';
+export const keepService = {
+    query,
+    remove,
+    save,
+    getById,
+    getEmptyNote,
+};
+
+const gNotes = [{
+        id: utilService.makeId(),
+        type: "note-txt",
+        isPinned: true,
+        info: {
+            txt: "Fullstack Me Baby!"
+        }
+    },
+    {
+        id: utilService.makeId(),
+        type: "note-img",
+        info: {
+            url: "http://some-img/me",
+            title: "Bobi and Me"
+        },
+        style: {
+            backgroundColor: "#00d"
+        }
+    },
+    {
+        id: utilService.makeId(),
+        type: "note-todos",
+        info: {
+            label: "Get my stuff together",
+            todos: [
+                { txt: "Driving liscence", doneAt: null },
+                { txt: "Coding power", doneAt: 187111111 }
+            ]
+        }
+    }
+];
+
+_createNotes();
+
+function query() {
+    return storageService.query(NOTES_KEY);
+}
+
+function remove(noteId) {
+    return storageService.remove(NOTES_KEY, noteId);
+}
+
+function save(note) {
+    if (note.id) return storageService.put(NOTES_KEY, note);
+    else return storageService.post(NOTES_KEY, note);
+}
+
+function getById(noteId) {
+    return storageService.get(NOTES_KEY, noteId);
+}
+
+function getEmptyNote() {
+    return {
+        id: storageService.makeId(),
+        type: '',
+    };
+}
+
+function _createNotes() {
+    let notes = utilService.loadFromStorage(NOTES_KEY)
+    if (!notes || !notes.length) {
+        notes = gNotes
+        utilService.saveToStorage(NOTES_KEY, notes)
+    }
+    return notes;
+}
