ab4c176f4ddc4a649e1e4fde133e96fd27da061f
Einav Cohen <einavsprojects@gmail.com>
2021-11-10 21:40:25 +0200

    seventh-commit

--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -2,7 +2,7 @@
 export default {
     template:`<section class="app-main">
         <div class="home-img">
-            <h1>Appsus<h1>
+            <h1>Appsus</h1>
                 <h2>Making Your Life Easier</h2>
                 <h2>With Just a Simple Touch Of a Finger</h2>
         </div>
