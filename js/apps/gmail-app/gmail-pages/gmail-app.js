f35c2d2b00189131b3a775564975cceeb728f3f8
Einav Cohen <einavsprojects@gmail.com>
2021-11-10 14:36:42 +0200

    changed css links in indez.html

--- js/apps/gmail-app/gmail-pages/gmail-app.js
+++ js/apps/gmail-app/gmail-pages/gmail-app.js
@@ -1,6 +1,6 @@
 
 export default {
     template:`<section class="gmail-app app-main">
-        <h1>Gmail App</h1>
+        <h1>Hi Gmail App</h1>
     </section>`
 }
---
+++ js/apps/gmail-app/services/gmail-service.js
@@ -0,0 +1,26 @@
+
+export const gmailService = {
+
+}
+
+const email = {
+    id: 'e101',
+    subject: 'Miss you!',
+    body: 'Would love to catch up sometimes',
+    isRead: false,
+    sentAt : 1551133930594,
+    to: 'momo@momo.com'
+}
+
+const loggedinUser = {
+    email: 'user@appsus.com',
+    fullname: 'Mahatma Appsus'
+}
+
+const criteria = {
+    status: 'inbox/sent/trash/draft',
+    txt: 'puki', // no need to support complex text search
+    isRead: true, // (optional property, if missing: show all)
+    isStared: true, // (optional property, if missing: show all)
+    lables: ['important', 'romantic'] // has any of the labels
+}
